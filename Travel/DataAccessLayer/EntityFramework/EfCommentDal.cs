9022522d1daccfb5c3076007fa3f7f62f6379c2e
Can Demir <84686864+Cndmr@users.noreply.github.com>
2022-09-08 17:43:34 +0300

    Console
    Console.

---
+++ Travel/DataAccessLayer/EntityFramework/EfCommentDal.cs
@@ -0,0 +1,47 @@
+﻿using DataAccessLayer.Abstract;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.Repositories;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.EntityFramework
+{
+    public class EfCommentDal : GenericRepository<Comment>, ICommentDal
+    {
+        public void CommentListWithLocationAndMember()
+        {
+           
+            Context c = new Context();
+            var yorumlar =  (from x in c.Comments
+                             join y in c.Locations
+                             on
+                             x.LocationId equals y.LocationId
+                             join z in c.Members
+                             on
+                             x.MemberId equals z.MemberId
+
+                             select new
+                        {
+                             x.CommentId,
+                             y.LocationName,
+                             z.MemberName,
+                             z.MemberSurname,
+                             x.CommenContent
+                          }).ToList();
+
+            foreach (var item in yorumlar)
+            {
+                Console.WriteLine(item.CommentId + " | " + item.LocationName + " | " + item.MemberName + "  " + item.MemberSurname + " | " +
+                    item.CommenContent);
+
+            }
+
+
+        }
+    }
+}
---
+++ Travel/Travel/Program.cs
@@ -0,0 +1,108 @@
+﻿using BusinessLayer.Concrete;
+using BusinessLayer.FluentValidation;
+using DataAccessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Dersleri_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //LocationManager locationManager = new LocationManager(new EfLocationDal());
+            //var values = locationManager.TGetList();
+            //void listele()
+            //{
+
+
+            //    foreach (var value in values)
+            //    {
+            //        Console.WriteLine(value.LocationName);
+            //        Console.WriteLine("----------");
+            //    }
+            //}
+            //Location location = new Location();
+
+            //EKLEME İŞLEMİ
+            ////location.LocationName = "Rotterdam";
+            ////locationManager.TInsert(location);
+            ///
+            //Silme İşlemi
+            //var locationValue=locationManager.TGetById(11);
+            //locationManager.TDelete(locationValue);
+
+            //Güncelleme
+            //var locationValue = locationManager.TGetById(2);
+            //locationValue.LocationName = "Çanakkale";
+            //locationManager.TUpdate(locationValue);
+            //Console.Write("Güncelleme Yapıldı");
+            //MemberManager memberManager= new MemberManager(new EfMemberDal());
+            //var values = memberManager.TGetList();
+            //foreach (var value in values)
+            //{
+            //    Console.WriteLine(value.MemberName + " " + value.MemberSurname);
+            //    Console.WriteLine("------------------");
+            //}
+
+            //Member member = new Member();
+            //member.MemberName = "a";
+            //member.MemberSurname = "s";
+            //memberManager.TInsert(member);
+            //var valueMember = memberManager.TGetById(9);
+            //memberManager.TDelete(valueMember);
+            //foreach (var item in valueMember)
+
+            //{
+            //    Console.WriteLine(item.);
+
+            //}
+
+            //valueMember.MemberName = "Aslı";
+            //valueMember.MemberSurname = "Meşe";
+            //memberManager.TUpdate(valueMember);
+
+
+            //Console.WriteLine("İşlem Yapıldı");
+            //CommentManager commentManager = new CommentManager(new EfCommentDal());
+            //var values = commentManager.TGetList();
+            //foreach (var value in values)
+            //{
+            //    Console.WriteLine(value.CommentId + "-");
+            //    Console.WriteLine("-----------");
+            //}
+
+            //CommentManager commentManager = new CommentManager(new EfCommentDal());
+            //commentManager.TCommentListWithLocationAndMember();
+
+            //MemberManager memberManager = new MemberManager (new EfMemberDal());
+            //Member member = new Member();
+            //member.MemberName = "Cemil";
+            //member.MemberSurname = "Kaya";
+            //MemberValidator validationRules = new MemberValidator();
+            //ValidationResult results= validationRules.Validate(member);
+            //if(results.IsValid)
+            //{
+            //    memberManager.TInsert(member);
+            //    Console.WriteLine("Üye başarılı bir şekilde eklendi");
+            //}
+            //else
+            //{
+            //    foreach (var item in results.Errors)
+            //    {
+            //        Console.Write(item.ErrorMessage);
+            //    }
+            //}
+           
+            Console.ReadLine();
+
+        }
+    }
+}
